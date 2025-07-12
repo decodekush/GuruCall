333af55def6172d64f9d2769847fe46bd8256284
decodekush <kushagraagarwal1612@gmail.com>
2025-07-13 04:10:03 +0530

    minor change

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { createClient } from '@deepgram/sdk';
-import { twiml as twimlLib } from 'twilio';
+import twilio from 'twilio';
 
+const { twiml: twimlLib } = twilio;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
